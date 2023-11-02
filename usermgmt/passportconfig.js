15f66989c9db30d63f85a1b9bc826167d68a5d78
PavPrasad <86524063+PavPrasad@users.noreply.github.com>
2023-11-02 14:56:18 +0530

    skadjfhaksjd

--- usermgmt/passportconfig.js
+++ usermgmt/passportconfig.js
@@ -1,40 +1,45 @@
 const express = require('express');
 const passport = require('passport');
-const { GetOauthUser, GetTempOauthUser, AddTempOauthUser } = require('../crud/crud')
+const { GetOauthUser, GetTempOauthUser, AddOauthUser } = require('../crud/crud')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://' + process.env.DOMAIN_NAME+'/auth/google/callback',
+    callbackURL: 'https://' + process.env.DOMAIN_NAME + '/auth/google/callback',
     scope: ['profile', 'email'],
     accessType: 'offline',
     prompt: 'consent'
-}, async (profile, done) => {
-        console.log(profile);
-        /*const email = profile.emails[0].value;
-        const displayname = profile.displayName;
-        const user = {
-            id: profile.id,
-            displayname,
-            email
-        };*/
-        const ttl = 3600;
-        GetOauthUser(user.id)
-            .then((message) => {
-                //person already present
-                console.log(message);
-                done(null, message);
-            })
-            .catch((error) => {
-                const msg = AddTempOauthUser(user.id, user.displayname, user.email, ttl);
-                console.log(msg);
-                GetTempOauthUser(user.id).then((message) => { done(null, message) }).catch((err) => { });
-            })
+}, async (accessToken, refreshToken, profile, done) => {
+    req.session.message = profile;
+
+    const email = profile.emails[0].value;
+    const displayname = profile.displayName;
+    const user = {
+        id: profile.id,
+        displayname,
+        email
+    };
+    const ttl = 3600;
+    req.session.user = user;
+    console.log(req.session.user);
+    GetOauthUser(user.id)
+        .then((message) => {
+            req.session.message = message;
+            console.log(message);
+            done(null, message);
+        })
+        .catch((error) => {
+            const msg = AddOauthUser(user.id, user.displayname, user.email, ttl);
+            console.log(msg);
+            req.session.message = msg;
+            done(null, msg);
+        })
+    console.log(`done for ${user.displayname} `);
     console.log(`done for ${user.displayname} `);
 
 }));
-/*
+
 passport.serializeUser(function (user, cb) {
     process.nextTick(function () {
         cb(null, { id: user.id, email: user.email, displayname: user.displayname });
@@ -46,35 +51,9 @@ passport.deserializeUser(function (user, cb) {
         return cb(null, user);
     });
 });
-*/
+
 
 module.exports = {
     passport
 };
 
-/*    console.log(profile);
-    req.session.message = profile;
-    *//*
-const email = profile.emails[0].value;
-const displayname = profile.displayName;
-const user = {
-    id: profile.id,
-    displayname,
-    email
-};
-const ttl = 3600;
-req.session.user = user;
-console.log(req.session.user);
-GetOauthUser(user.id)
-    .then((message) => {
-        req.session.message = message;
-        console.log(message);
-        done(null, message);
-    })
-    .catch((error) => {
-        const msg = AddOauthUser(user.id, user.displayname, user.email,ttl);
-        console.log(msg);
-        req.session.message = msg;
-        done(null, msg);
-    })*//*
-console.log(`done for ${user.displayname} `);git */
